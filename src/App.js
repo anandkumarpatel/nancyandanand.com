c3282649abd6ad85e1d1964505daf9184765ac39
Anand Patel <anandkumarpatel@gmail.com>
2020-01-25 18:22:06 -0500

    add cards and tix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import CardColumns from 'react-bootstrap/CardColumns'
 import Image from 'react-bootstrap/Image'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -149,7 +150,7 @@ class App extends Component {
     const eventCard = (place, name, date, times, loc, desc) => {
       return (
         <Row className="event">
-          <Col sm className="map-parent">
+          <Col sm={4} className="map-parent">
             <a href="google.com">
               <div className="map-overlay" />
             </a>
@@ -162,7 +163,6 @@ class App extends Component {
           <Col className="map-info">
             <h1>{name}</h1>
             <div className="text-detail">
-              <br />
               {date}: {times} <br />
               {loc} <br />
             </div>
@@ -177,78 +177,163 @@ class App extends Component {
     const events = () => {
       return (
         <div className="section white">
-          <div className="content-center">
-            <Container className="events">
-              <h1>Events</h1>
-              {eventCard(
-                'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
-                'Welcome Dinner & Garba',
-                'April 3rd',
-                '7:00pm - 10:00pm',
-                'Ashiana: 5675 Jimmy Carter Blvd #101, Norcross, GA 30071',
-                "Prepare to eat and dance the night away. This is your chance to mingle with the guest before the big day. Garba is a form of Indian dance. If you don't know it is quick to pick up. Dress colorful"
-              )}
+          <Container className="events">
+            <h1>Events</h1>
+            {eventCard(
+              'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
+              'Garba',
+              'April 3rd',
+              '7:00 pm',
+              'Ashiana: 5675 Jimmy Carter Blvd, Norcross, GA',
+              "This is your chance to mingle with the guest before the big day. Garba is a form of Indian dance. If you don't know it is quick to pick up. Dress colorful"
+            )}
 
-              {eventCard(
-                'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
-                'Wedding',
-                'April 4th 2020',
-                '10:00am - 3:00pm',
-                'Park Tavern: 500 10th St NE, Atlanta, GA 30309',
-                'We will start with the Bharat which is the grooms procession to the venue. Once at the Venue we will have a short Hindu ceremony followed by a lunch. Dress Indian'
-              )}
+            {eventCard(
+              'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
+              'Wedding',
+              'April 4th 2020',
+              '10:00 am',
+              'Park Tavern: 500 10th St NE, Atlanta, GA',
+              'We will start with the Bharat which is the grooms procession to the venue. Once at the Venue we will have a short Hindu ceremony followed by a lunch. Dress Indian'
+            )}
 
-              {eventCard(
-                'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
-                'Reception',
-                'April 4th 2020',
-                '7:00pm - 12:00pm',
-                'Fox Theater:  660 Peachtree St NE, Atlanta, GA 30308',
-                'This is it, the final event. There will be food, performances and more dancing. Dress sharp'
-              )}
-            </Container>
-          </div>
+            {eventCard(
+              'place_id:ChIJZ3_vhMem9YgRX3--sf9DM3Y',
+              'Reception',
+              'April 4th 2020',
+              '7:00 pm',
+              'Fox Theater:  660 Peachtree St NE, Atlanta, GA',
+              'This is it, the final event. There will be food, performances and more dancing. Dress sharp'
+            )}
+          </Container>
         </div>
       )
     }
-    const things = [
+    const food = [
       {
-        name: 'stk',
-        desc: 'great palce to eat',
-        link: ''
+        name: 'STK',
+        desc: 'Amazing steak & drinks.',
+        link: 'https://stksteakhouse.com/venues/atlanta/#venue-menu-section',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipM2Ty9gW29dpeZTpDuZQFvEs2WHTZ9yzfsBBxHF=h512'
       },
       {
-        name: 'beltline',
-        desc: 'great to walk',
-        link: ''
+        name: 'Krog Street Market',
+        desc: 'Lots of food vendors',
+        link: 'https://krogstreetmarket.com/',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipPBAkBiejx3sqV4LjFtHT561USwqvfzuND41WpE=h512'
+      },
+      {
+        name: 'A Mano',
+        desc: 'Best Italian resturant',
+        link: 'https://www.amanoatl.com/',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipMcHOiu7FhxRh4xg0R8Fz7P9RIH-5pB1zYcoR96=h512'
+      },
+      {
+        name: 'Alma Cocina',
+        desc: 'Best Mexican resturant',
+        link: 'https://krogstreetmarket.com/',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipN1mmcWPcesZIZ0sZo6G5RqSmuYlIoVHBIiN-e-=h512'
+      },
+      {
+        name: 'Nina & Rafi',
+        desc: 'Best Pizza',
+        link: 'https://www.ninaandrafi.com/',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipOTBfICmo2YLuGr5mIBm6yhLYzQ2UPKBpXaZ2w3=h512'
+      },
+      {
+        name: 'Dairies',
+        desc: 'Best Cafe',
+        link: 'https://www.coldbrewbar.com/',
+        img:
+          'https://lh5.googleusercontent.com/p/AF1QipN-rJGESdMiXaBq-2v-P5tnKA1W163To3iQAwWu=h512'
       }
     ]
 
-    const thing = (thing) => {
+    const activities = [
+      {
+        name: 'Georgia Aquarium',
+        desc: 'See fish',
+        link: 'https://www.georgiaaquarium.org/',
+        img:
+          'https://www.georgiaaquarium.org/wp-content/uploads/2018/07/whale-1-300x143@2x.png'
+      },
+      {
+        name: 'World of Coke',
+        desc: 'See where Coke is made',
+        link: 'https://www.worldofcoca-cola.com/',
+        img: 'http://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c0e3.png'
+      },
+      {
+        name: 'Ponce City Market',
+        desc: 'Shopping & Eatting in an old Ford Factory',
+        link: 'http://www.poncecitymarket.com/',
+        img:
+          'https://poncecitymarket.com/wp-content/themes/poncecitymarket/assets/images/header_icon.png'
+      },
+      {
+        name: 'High Museum of Art',
+        desc: 'See Pictures',
+        link: 'https://high.org/',
+        img:
+          'https://high.org/wp-content/themes/base-theme/assets/logo/high-logo.svg'
+      },
+      {
+        name: 'Beltline',
+        desc: 'great to walk',
+        link: 'https://beltline.org/',
+        img:
+          'https://beltlineorg-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/videothumb.jpg'
+      },
+      {
+        name: 'Atlantic Station',
+        desc: 'Outdoor mall',
+        link: 'https://www.atlanticstation.com/',
+        img: 'https://assets.speakcdn.com/assets/2500/skyview-2.jpg'
+      },
+      {
+        name: 'Final Four',
+        desc: 'Sports!',
+        link: 'https://www.ncaa.com/final-four',
+        img:
+          'https://www.ncaa.com/sites/default/files/public/styles/original/public-s3/tile-images/franchise_hero/logos/20_MBB_FinalFour_FC_RGB%4072_0.png?itok=MRj9xwGW'
+      }
+    ]
+
+    const createThingCard = (thing) => {
       const name = thing.name
       const desc = thing.desc
       const link = thing.link
+      const img = thing.img
       return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        // <Col>
+        <Card bg="dark" text="white" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={img} />
           <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>{desc}</Card.Text>
-            <Card.Link href={link}>Details</Card.Link>
+            <Card.Link href={link}>Explore</Card.Link>
           </Card.Body>
         </Card>
+        // </Col>
       )
     }
 
     const thingsToDo = () => {
       return (
-        <div className="section pink">
-          <div className="content-center">
-            <Container>
-              <h1>Have your own Adventure while your in Atlanta </h1>
-              {things.map(thing)}
-            </Container>
-          </div>
+        <div className="section todo pink">
+          <Container>
+            <h1>Have your own Adventure while your in Atlanta </h1>
+            <h2> Food </h2>
+            <CardColumns>{food.map(createThingCard)}</CardColumns>
+
+            <h2> Activities </h2>
+            <CardColumns>{activities.map(createThingCard)}</CardColumns>
+          </Container>
         </div>
       )
     }
@@ -258,7 +343,10 @@ class App extends Component {
       width: '640',
       playerVars: {
         autoplay: 0,
-        controls: 0
+        controls: 1,
+        iv_load_policy: 3,
+        modestbranding: 1,
+        rel: 0
       }
     }
 
@@ -284,13 +372,11 @@ class App extends Component {
         </div>
         {/* {navBar()} */}
         <div className="section pink">
-          <div className="content-center">
-            <Container>
-              <h1>Nancy & Anand </h1>
-              <h2>Are getting married</h2>
-              <h2>April 4th 2020</h2>
-            </Container>
-          </div>
+          <Container>
+            <h1>Nancy & Anand </h1>
+            <h2>Are getting married</h2>
+            <h2>April 4th 2020</h2>
+          </Container>
         </div>
         {events()}
         {thingsToDo()}
